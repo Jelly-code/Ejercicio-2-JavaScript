5e04e9ce742d0dfca9e912673945acc25f0d3d57
Jelly-code <santamaria.javiera@gmail.com>
2020-08-19 11:15:17 -0400

    Ejercicio 2 JS

---
+++ main.js
@@ -0,0 +1,7 @@
+let saborDeHelado1 = prompt("Bienvenida, dime tu sabor de helado favorito")
+let saborDeHelado2 = prompt("Y el segundo?")
+let saborDeHelado3 = prompt("Y el tercero?")
+
+let heladoCompleto = saborDeHelado1 + ", " + saborDeHelado2 + " y " + saborDeHelado3
+
+alert("Aquí tiene su helado de " + heladoCompleto + ". Gracias por su visita")
